96e5ae534ae2d90b4ec4f9ba0a93240e9522478e
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-13 00:43:28 -0800

    commit tree 6cd9826418ae214aa4931594adae906fc7d01b19

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          console.log('Now')
+          Now
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
